86c1ed3564d2b36e8fe98964765d26b13e134996
João Coelho <joaoantonioscoelho@gmail.com>
2023-01-09 22:34:48 -0300

    config chakra's theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import {Inter} from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-  return (
-      <>
-        <h1>Dashgo</h1>
-      </>
-  )
+    return (
+        <>
+            <h1>Dashgo</h1>
+        </>
+    )
 }
