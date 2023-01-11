6bad02d08f37bf75dec169ebcafdf90598a305bb
João Coelho <joaoantonioscoelho@gmail.com>
2023-01-10 21:59:51 -0300

    fix links on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,11 +9,11 @@ export function Sidebar() {
                 <Box>
                     <Text fontWeight={'bold'} color={'gray.400'} fontSize={'small'}>GERAL</Text>
                     <Stack spacing={'4'} mt={'8'} align={'stretch'}>
-                        <Link display={'flex'} align={'center'}>
+                        <Link display={'flex'}>
                             <Icon as={RiDashboardLine} fontSize={'20'} />
                             <Text ml={'4'} fontWeight={'medium'}>Dashboard</Text>
                         </Link>
-                        <Link display={'flex'} align={'center'}>
+                        <Link display={'flex'}>
                             <Icon as={RiContactsLine} fontSize={'20'} />
                             <Text ml={'4'} fontWeight={'medium'}>Usuários</Text>
                         </Link>
@@ -23,11 +23,11 @@ export function Sidebar() {
                 <Box>
                     <Text fontWeight={'bold'} color={'gray.400'} fontSize={'small'}>AUTOMAÇÃO</Text>
                     <Stack spacing={'4'} mt={'8'} align={'stretch'}>
-                        <Link display={'flex'} align={'center'}>
+                        <Link display={'flex'}>
                             <Icon as={RiInputMethodLine} fontSize={'20'} />
                             <Text ml={'4'} fontWeight={'medium'}>Formulários</Text>
                         </Link>
-                        <Link display={'flex'} align={'center'}>
+                        <Link display={'flex'}>
                             <Icon as={RiContactsLine} fontSize={'20'} />
                             <Text ml={'4'} fontWeight={'medium'}>Automação</Text>
                         </Link>
