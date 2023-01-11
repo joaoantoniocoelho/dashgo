7ae3d872624cf91b1fb1bd8e6a2ca30a55470ecf
João Coelho <joaoantonioscoelho@gmail.com>
2023-01-11 20:29:09 -0300

    add active link

--- src/components/sidebar/NavLink.tsx
+++ src/components/sidebar/NavLink.tsx
@@ -1,20 +1,20 @@
 import {Icon, Link as ChakraLink, LinkProps, Text} from "@chakra-ui/react";
-import {RiContactsLine} from "react-icons/ri";
 import React, {ElementType} from "react";
-import Link from "next/link";
+import {ActiveLink} from "../ActiveLink";
 
-interface NavLinkProps extends LinkProps{
+interface NavLinkProps extends LinkProps {
     icon: ElementType;
     children: string;
     href: string;
 }
+
 export function NavLink({icon, children, href, ...rest}: NavLinkProps) {
-    return(
-        <Link href={href} passHref>
+    return (
+        <ActiveLink href={href} passHref>
             <ChakraLink display={'flex'} {...rest}>
-                <Icon as={icon} fontSize={'20'} />
+                <Icon as={icon} fontSize={'20'}/>
                 <Text ml={'4'} fontWeight={'medium'}>{children}</Text>
             </ChakraLink>
-        </Link>
+        </ActiveLink>
     )
 }
